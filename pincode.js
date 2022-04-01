0635528a8e8ffe61f3d357cf7f24755263e049ef
Bhavesh2302 <jainbhavesh888@gmail.com>
2022-04-01 16:34:26 +0530

    added Homepage

---
+++ pincode.js
@@ -0,0 +1,18 @@
+document.querySelector("form").addEventListener("submit",pincode)
+
+
+var enteredpincode=localStorage.getItem("pincode")
+function pincode(){
+    var enteredpincode=document.querySelector("#enterpincode").value;
+console.log("inside function")
+
+    if(enteredpincode.length=6){
+        window.location.href="index.html"
+    }
+
+    else{
+        alert("Enter a Valid Pincode")
+    }
+localStorage.setItem("pincode",enteredpincode)
+
+}
